d74b35df704f1c37c2a4285dcb023a2cb908e28a
chengweijie <chengweijie@chengweijiedeMacBook-Pro.local>
2019-08-21 13:13:03 +0800

    Initial commit

---
+++ src/main/java/com/abc/consumer/ConsumerApplication8090.java
@@ -0,0 +1,17 @@
+package com.abc.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+// 指定Service接口所在的包，开启OpenFeign客户端
+@EnableFeignClients(basePackages = "com.abc.consumer.service")
+@SpringCloudApplication
+public class ConsumerApplication8090 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication8090.class, args);
+        System.out.println(8090);
+    }
+
+}
